ebc1292a24fcea085838f37ce1c287737fd02960
JoChuan <ruochuanyang@gmail.com>
2025-12-17 22:18:12 +0800

    edit home page text size
    20251217

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
 
         <div className="flex w-[90%] justify-between">
 
-          <div className="w-[65%] mb-8">
-            <div className="text-[40px] md:text-[64px] font-black">Hello, <br />I'm Jo-Chuan.</div>
-            <div className="text-[12px] md:text-[16px] font-medium leading-6 md:leading-8 mt-6">會設計、也懂一點程式語言；<br />對新科技有濃厚興趣，也樂於自學技術。<br />期望透過數位設計對社會有所貢獻。</div>
+          <div className="w-full md:w-[65%] mb-8">
+            <div className="text-[48px] md:text-[64px] font-black">Hello, <br />I'm Jo-Chuan.</div>
+            <div className="text-[14px] md:text-[16px] font-medium leading-8 md:leading-8 mt-6">會設計、也懂一點程式語言；<br />對新科技有濃厚興趣，也樂於自學技術。<br />期望透過數位設計對社會有所貢獻。</div>
           </div>
 
         </div>
