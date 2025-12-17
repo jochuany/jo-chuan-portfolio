8a0a3c289a6e9fe4a332fd18e2b7be289e12643c
JoChuan <ruochuanyang@gmail.com>
2025-12-18 00:26:17 +0800

    edit contact page layout
    20251218

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -7,9 +7,31 @@ export default function Contact() {
 
         <div className="flex w-[90%] justify-between">
 
-          <div className="w-[65%] mb-8">
-            <div className="text-[40px] md:text-[64px] font-black">Contact me!</div>
-            <div className="text-[12px] md:text-[16px] font-bold leading-6 md:leading-8 mt-6">Email: ruochuanyang@gmail.com<br />Mobile: 0978-193-380</div>
+          <div className="w-full md:w-[65%]">
+            <div className="text-[40px] md:text-[64px] font-black mb-10">Contact me!</div>
+
+            <div className="text-[16px]">
+
+              <div className="mb-8">
+                <div className="font-medium">
+                  Email
+                </div>
+                <div className="font-bold">
+                  ruochuanyang@gmail.com
+                </div>
+              </div>
+
+              <div className="mb-8">
+                <div className="font-medium">
+                  Mobile
+                </div>
+                <div className="font-bold">
+                  0978-193-380
+                </div>
+              </div>
+
+            </div>
+
           </div>
 
         </div>
