a14fe7e854c4e9563dbfa5f3ca723bff14e12ba0
JoChuan <ruochuanyang@gmail.com>
2025-12-17 22:16:18 +0800

    create vnccu page
    20251217

---
+++ src/app/projects/vnccu/page.js
@@ -0,0 +1,73 @@
+import Image from "next/image";
+
+export default function Vnccu() {
+  return (
+    <>
+      <div className="flex w-screen h-auto pt-10 pb-10 justify-center items-start text-[#1a2690] bg-[#f2efdf]">
+
+        <div className="flex flex-col w-[90%] justify-between">
+
+
+
+          <div className="text-[24px] font-bold mb-6 md:text-[26px]">
+            政大之聲開播週
+          </div>
+
+          <div className="flex flex-col w-full mb-6 md:flex-row md:justify-between text-[14px] md:text-[16px]">
+
+            <div className="mb-4 md:w-[30%]">
+              <div className="font-medium">
+                TYPE
+              </div>
+              <div className="font-bold">
+                主視覺設計
+              </div>
+            </div>
+
+            <div className="mb-4 md:w-[30%]">
+              <div className="font-medium">
+                YEAR
+              </div>
+              <div className="font-bold">
+                2021
+              </div>
+            </div>
+
+            <div className="mb-4 md:w-[30%]">
+              <div className="font-medium">
+                TOOL
+              </div>
+              <div className="font-bold">
+                Adobe Illustrator
+              </div>
+            </div>
+
+          </div>
+
+          <div className="w-full flex flex-col">
+            <div className="md:w-[65%] text-justify text-[12px] md:text-[14px] mb-6 leading-6">
+              大學時期於政大之聲實習電台為活動設計的主視覺，以繽紛、明亮風格呈現，象徵電台節目的多元性。由個人獨立完成所有圖像的繪製與設計，包括 Facebook 大頭貼、宣傳 Banner、以及實體海報。
+            </div>
+            <a href="https://www.facebook.com/share/p/1L6hnGVNay/" target="_blank"
+              className="text-[12px] text-[#f2efdf] text-center mb-6 py-2 md:text-[14px] md:w-[30%]
+              bg-[#1a2690] border hover:bg-[#f2efdf] hover:text-[#1a2690] hover:border-[#1a2690] transition-all duration-300 ease">
+              點此看作品
+            </a>
+          </div>
+
+          <div className="w-full grid grid-cols-1 gap-6 md:grid-cols-2">
+            <img src="/images/vnccu/vnccu-01.jpg" className="w-full md:col-span-2" />
+            <img src="/images/vnccu/vnccu-02.jpg" className="w-full md:col-span-2" />
+            <img src="/images/vnccu/vnccu-03.jpg" className="w-full md:col-span-1" />
+            <img src="/images/vnccu/vnccu-04.jpg" className="w-full md:col-span-1" />
+            <img src="/images/vnccu/vnccu-05.jpg" className="w-full md:col-span-1" />
+            <img src="/images/vnccu/vnccu-06.jpg" className="w-full md:col-span-1" />
+            <img src="/images/vnccu/vnccu-07.jpg" className="w-full md:col-span-1" />
+          </div>
+
+        </div>
+
+      </div>
+    </>
+  );
+}
