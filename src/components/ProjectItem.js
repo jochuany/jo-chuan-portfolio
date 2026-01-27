53e48c3b6a0bfa49d39dc3be94c2e907399985d9
JoChuan <ruochuanyang@gmail.com>
2026-01-27 23:21:22 +0800

    fix bugs
    20260127

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,22 +6,22 @@ export const ProjectItem = ({ href, title, category, year, photo, tools }) => {
         <a href={href} className="md:border-none transition-all duration-300 ease group">
             <div className="flex flex-col justify-between items-start transition-all duration-300 ease">
                 <div className="h-[160px] md:h-[180px] lg:h-[200px] w-full mb-2 overflow-hidden relative group">
-                    <div style={{ backgroundImage: `url(${photo})` }} className="h-full w-full md:grayscale
-                bg-cover bg-center bg-no-repeat transition-all duration-500 ease md:group-hover:scale-110 md:group-hover:grayscale-0"></div>
+                    <div style={{ backgroundImage: `url(${photo})` }} className={`h-full w-full md:grayscale
+                bg-cover bg-center bg-no-repeat transition-all duration-500 ease md:group-hover:scale-110 md:group-hover:grayscale-0`}></div>
                 </div>
-                <div className="text-[18px] transition-all duration-300 ease font-bold mb-2
-                md:group-hover:text-brand-primary">
+                <div className={`text-[18px] transition-all duration-300 ease font-bold mb-2
+                md:group-hover:text-brand-primary`}>
                     {title}
                 </div>
-                <div className="text-[16px] transition-all duration-300 ease font-normal mb-2 md:group-hover:text-brand-primary">
+                <div className={`text-[16px] transition-all duration-300 ease font-normal mb-2 md:group-hover:text-brand-primary`}>
                     {category}{"／"}{year}
                 </div>
-                <div className="flex flex-wrap gap-2 mb-3">
+                <div className={`flex flex-wrap gap-2 mb-3`}>
                     {tools && tools.length > 0 && tools.map((item, index) => (
                         <div
                             key={index}
-                            className="text-[14px] px-2 py-0.5 bg-brand-graybg inset-shadow-sm rounded-full transition-all duration-300 ease
-                            md:group-hover:bg-brand-primaryLight md:group-hover:text-brand-bg"
+                            className={`text-[14px] px-2 py-0.5 bg-brand-graybg inset-shadow-sm rounded-full transition-all duration-300 ease
+                            md:group-hover:bg-brand-primaryLight md:group-hover:text-brand-bg`}
                         >
                             {item}
                         </div>
