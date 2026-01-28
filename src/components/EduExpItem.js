06de9356b335bc5a9f6b949d518304c03b85690b
JoChuan <ruochuanyang@gmail.com>
2026-01-28 11:50:02 +0800

    fix bug
    20260128

--- src/components/EduExpItem.js
+++ src/components/EduExpItem.js
@@ -3,7 +3,7 @@
 export const EduExpItem = ({ title, place, time, description }) => {
     return (
         <div className="mb-6 lg:hover:text-brand-primary transition-all duration-300 ease">
-            <div className="font-medium">{title}</div>
+            <div className="font-medium whitespace-pre-line">{title}</div>
             <div className="flex justify-between font-normal">
                 <div>{place}</div>
                 <div>{time}</div>
