2c06661df64b7270ee111b142945d3cdf9292c18
JoChuan <ruochuanyang@gmail.com>
2026-02-05 11:44:22 +0800

    update next and react, update about skills
    20260205

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,31 +8,38 @@ export const metadata = {
 const skills = [
   {
     title: "視覺設計與資訊動畫",
+    skillDescription: "可獨立撰寫敘事腳本並規劃分鏡、設計轉場、製作素材，將抽象的議題以視覺化、動態化的方式呈現，使其更具吸引力。",
     tools: ["Adobe Illustrator", "Adobe Photoshop", "Adobe After Effects", "Adobe Audition"]
   },
   {
     title: "使用者體驗與介面設計",
+    skillDescription: "可進行使用者研究發掘痛點，並製作清楚的流程圖、線框稿與原型。具備設計心理學與人機互動基礎知識、熟悉傳播理論，助於應用在設計考量與產品策略中。",
     tools: ["Figma", "Miro", "User Research", "Flowchart", "Wireframing", "Prototyping"]
   },
   {
     title: "前端網頁開發",
-    tools: ["HTML", "CSS", "JavaScript", "RWD", "Tailwind CSS", "Next.js", "React Hooks", "Vibe Coding"]
+    skillDescription: "熟悉前端語言，能將設計稿精準轉化為具響應式設計的網頁。能使用現代框架與函式庫進行開發，並能運用元件庫開發微互動網頁。",
+    tools: ["HTML", "CSS", "JavaScript", "RWD", "Tailwind CSS", "Next.js", "React Hooks", "Vibe Coding", "Cursor", "VS Code", "Github"]
   },
   {
     title: "排版編輯與簡報設計",
+    skillDescription: "能使用排版編輯軟體，將複雜資訊簡化為易於理解的視覺化簡報，並曾編輯過雜誌、手冊等出版品。",
     tools: ["Adobe InDesign", "PowerPoint", "Keynote", "Canva"]
   },
   {
     title: "問卷設計與統計分析",
+    skillDescription: "了解如何設計良好的調查問卷，並將蒐集到的數據進行清理與分析。曾使用 Chrome 插件工具爬取網路資料，並將數據轉化為視覺化圖表。",
     tools: ["Excel", "SPSS", "Web Scraper", "Flourish"]
   },
   {
     title: "攝影與影像後製",
+    skillDescription: "能根據不同主題設計影像風格並提出企劃，也能使用影像後製軟體進行修圖與調色。",
     tools: ["Adobe Lightroom", "Adobe Photoshop"]
   },
   {
     title: "遊戲開發",
-    tools: ["Unity"]
+    skillDescription: "曾合作開發開發 3D 遊戲，並能使用 Blender 建立 3D 模型。",
+    tools: ["Unity C#", "Unity UI", "Blender"]
   },
 ];
 
@@ -55,11 +62,11 @@ const exp = [
     place: "TLab 媒體心理實驗室",
     time: "2024/02 - 2025/10",
     description: [
-      "協助社會科學、媒體心理實驗法之研究進行，與助理群合作接待超過百位受試者，以確保數據品質，了解使用者在虛擬實境體驗中的心理行為。",
-      "曾拍攝與剪輯 360 全景影片，並擁有 XR 頭戴式顯示器之操作經驗。",
-      "協助沉浸科技、虛擬實境社交等主題之文獻搜集整理，掌握媒體心理學的研究趨勢。",
+      "協助 6 個社會科學／媒體心理學研究實驗法之進行，每個實驗皆與助理群合作接待 100+ 位受試者，以確保數據品質、了解使用者在虛擬實境體驗中的心理行為。",
+      "協助沉浸科技體驗、虛擬實境社交主題進行文獻搜集與整理，彙整並掌握媒體心理學領域之最新研究趨勢，與助理群合作累積整理文獻數量 30+ 篇。",
       "參與文化科技計畫：協助搜集國內外沉浸文化科技資料，包含各界定義、國內外成功案例等，並提出分類框架、根據產業架構整合臺灣文化科技產業地圖。",
-      "參與數位信任計畫：協助專家訪談資料整理，進行數位信任之文獻探討，了解數位信任之定義、不同領域的數位信任、提升數位信任的方法等，並撰寫成報告。",
+      "參與數位信任計畫：與助理群合作協助 18 位專家訪談資料整理，並進行 50+ 篇數位信任相關文獻探討，深入瞭解其定義及應用領域，並從科技面、人際面與個體差異 3 面向提出提升數位信任之建議，最終整合成果撰寫成研究報告。",
+      "曾拍攝與剪輯 360 全景影片，並擁有 XR 頭戴式顯示器之操作經驗。",
     ]
   },
   {
@@ -67,10 +74,10 @@ const exp = [
     place: "政大之聲廣播電台",
     time: "2019/09 - 2022/06",
     description: [
-      "廣播節目製作：合作完成節目企劃、採訪、主持、剪輯、現場播出。",
-      "新聞資訊編輯：編輯國內外重要新聞稿，並於整點 Live 播報；帶領助理檢討專題稿件。",
-      "深度專題產製：合作擬訂專題企劃、採訪、撰寫、過音，完成 2000 字以上之深度專題報導。",
-      "視覺設計協助：參與開播活動視覺設計（含實體海報、線上社群）、帶狀節目視覺設計、節目主視覺設計。",
+      "廣播節目製作：合作完成 3 個節目企劃、採訪、主持、剪輯與現場播出。領導一 4 人小組發想節目企劃，並協助製播工程。",
+      "新聞資訊編輯：每週編輯大台北新聞、國內外重要新聞 5+ 篇，並於整點 Live 播報。指導 2 位見習助理檢討並優化專題稿件。",
+      "深度專題產製：每週召開線索會，並合作擬訂專題企劃、採訪、撰寫、過音，完成 10+ 篇 2000 字以上之深度專題報導。",
+      "視覺設計協助：合作參與 2 個、獨立製作 1 個活動主視覺設計，含實體海報與線上社群用圖，提升視覺一致性。",
     ]
   },
 ];
@@ -122,7 +129,7 @@ export default function About() {
                 {"// "}Education
               </div>
 
-              <div className="text-[16px] text-brand-text font-normal leading-8 flex flex-col">
+              <div className="text-[16px] text-brand-text font-normal leading-8 flex flex-col text-justify">
 
                 {edu.map((item, index) => (
                   <EduExpItem key={index} {...item} />
@@ -139,7 +146,7 @@ export default function About() {
                 {"// "}Experience
               </div>
 
-              <div className="text-[16px] text-brand-text font-normal leading-8 flex flex-col">
+              <div className="text-[16px] text-brand-text font-normal leading-8 flex flex-col text-justify">
 
                 {exp.map((item, index) => (
                   <EduExpItem key={index} {...item} />
@@ -156,7 +163,7 @@ export default function About() {
                 {"// "}Skills
               </div>
 
-              <div className="text-[16px] text-brand-text font-normal flex flex-col">
+              <div className="text-brand-text font-normal flex flex-col">
 
                 {skills.map((item, index) => (
                   <SkillItem key={index} {...item} />
--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -1,15 +1,18 @@
 // Skills 版型
 
-export const SkillItem = ({ title, tools }) => {
+export const SkillItem = ({ title, skillDescription, tools }) => {
     return (
-        <div className="mb-6 group">
-            <div className="text-brand-text font-medium leading-8 lg:group-hover:text-brand-primary transition-all ease duration-300">{title}</div>
+        <div className="mb-8 group lg:hover:-translate-y-1 transition-all duration-300 ease">
+            <div className="text-[16px] text-brand-text font-medium leading-8
+            lg:group-hover:text-brand-primary transition-all ease duration-300">{`${title}`}</div>
+            <div className="text-[14px] text-brand-text font-normal leading-7 text-justify mb-2
+            lg:group-hover:text-brand-primary/60 transition-all ease duration-300">{skillDescription}</div>
             <div className="flex flex-wrap gap-2 mt-2">
                 {tools.map((item, index) => (
                     <div
                         key={index}
                         className={`text-[12px] px-2 py-0.5 bg-brand-graybg inset-shadow-sm rounded-full
-                            border border-brand-graytext/20 transition-all duration-300 ease lg:group-hover:-translate-y-[2px]
+                            border border-brand-graytext/20 transition-all duration-300 ease
                             lg:group-hover:bg-brand-primaryLight/20 lg:group-hover:text-brand-primary lg:group-hover:border-brand-primaryLight
                             dark:lg:group-hover:bg-brand-primary/20 dark:lg:group-hover:text-brand-primaryLight dark:lg:group-hover:border-brand-primaryLight/70`}
                     >
